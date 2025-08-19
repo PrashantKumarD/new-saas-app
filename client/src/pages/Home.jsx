da7478bd07c3b468316b412cd3cba1dc06490489
PrashantKumarD <kumarprashant6080@gmail.com>
2025-08-19 19:04:46 +0530

    new build

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
-import AiTools from "../components/AiTools";
 import Plan from "../components/Plan";
 import Footer from "../components/Footer";
 
